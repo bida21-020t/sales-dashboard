1dae88c5421bff8b4a2f6917dc1cf44b0db084c0
bida21-020t <100626087+bida21-020t@users.noreply.github.com>
2025-05-23 13:19:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -508,4 +508,5 @@ def export_filtered_data(n_clicks, start_date, end_date, region, product, salesp
 
 # Run the app
 if __name__ == '__main__':
-    dash_app.run_server(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)), debug=False)
+    port = int(os.environ.get('PORT', 8050))
+    dash_app.run_server(host='0.0.0.0', port=port)
