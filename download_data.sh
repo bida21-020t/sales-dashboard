e02861f1b1a9acc649c201e6b45423e5f5cd3c97
bida21-020t <100626087+bida21-020t@users.noreply.github.com>
2025-05-23 03:47:46 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Google Drive file ID (replace below)
+FILE_ID="1NdErQNL4JcqBAQaOlLk8GzZLnzgjS8qp"
+OUTPUT="web_interaction_data_with_clusters.csv"
 
+wget --no-check-certificate \
+  "https://drive.google.com/uc?export=download&id=${1NdErQNL4JcqBAQaOlLk8GzZLnzgjS8qp}" \
+  -O "${OUTPUT}"
+echo "✅ Dataset downloaded!"
